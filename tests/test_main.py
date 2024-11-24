bf47c054d1b105438aef5edb9d811de263a099f1
Tamer Hamad Faour <tamer@tameronline.com>
2024-11-24 21:55:09 +0100

    CI_test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
-from fastapi.testclient import TestClient
+import sys
+import os
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app.main import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
